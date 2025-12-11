ee8720b5bd614aeaf3f4a36dbae7277bda14db4e
Joker <lieserdylan7@gmail.com>
2025-12-11 21:39:43 +0100

    Add files via upload

---
+++ KookBoek_Examen/KookBoek_Examen/Form5.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace KookBoek_Examen
+{
+    partial class HelpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.geschiedenisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keuzeMakenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.AliceBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.geschiedenisToolStripMenuItem,
+            this.keuzeMakenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // geschiedenisToolStripMenuItem
+            // 
+            this.geschiedenisToolStripMenuItem.Name = "geschiedenisToolStripMenuItem";
+            this.geschiedenisToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.geschiedenisToolStripMenuItem.Text = "Geschiedenis";
+            // 
+            // keuzeMakenToolStripMenuItem
+            // 
+            this.keuzeMakenToolStripMenuItem.Name = "keuzeMakenToolStripMenuItem";
+            this.keuzeMakenToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.keuzeMakenToolStripMenuItem.Text = "Keuze maken";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(589, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hier is het hulpdienst indein er iets misgaat: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(184, 234);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(401, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "04 91 13 44 40 (u eigen zoon)";
+            // 
+            // HelpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "HelpForm";
+            this.Text = "Help";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem geschiedenisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keuzeMakenToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ KookBoek_Examen/KookBoek_Examen/Form6.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace KookBoek_Examen
+{
+    partial class GeschiedenisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeschiedenisForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keuzeMakenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Geschiedenisdata = new System.Windows.Forms.DataGridView();
+            this.NaamGerecht = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Datum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AantalPersonen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tijdsduur = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Commentaar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Geschiedenisdata)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.AliceBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.keuzeMakenToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // keuzeMakenToolStripMenuItem
+            // 
+            this.keuzeMakenToolStripMenuItem.Name = "keuzeMakenToolStripMenuItem";
+            this.keuzeMakenToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.keuzeMakenToolStripMenuItem.Text = "Keuze maken";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Geschiedenisdata
+            // 
+            this.Geschiedenisdata.BackgroundColor = System.Drawing.Color.LightSlateGray;
+            this.Geschiedenisdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Geschiedenisdata.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NaamGerecht,
+            this.Datum,
+            this.AantalPersonen,
+            this.Tijdsduur,
+            this.Commentaar});
+            this.Geschiedenisdata.Location = new System.Drawing.Point(5, 29);
+            this.Geschiedenisdata.Name = "Geschiedenisdata";
+            this.Geschiedenisdata.Size = new System.Drawing.Size(546, 409);
+            this.Geschiedenisdata.TabIndex = 2;
+            // 
+            // NaamGerecht
+            // 
+            this.NaamGerecht.HeaderText = "Naam van Gerecht";
+            this.NaamGerecht.Name = "NaamGerecht";
+            // 
+            // Datum
+            // 
+            this.Datum.HeaderText = "Datum";
+            this.Datum.Name = "Datum";
+            // 
+            // AantalPersonen
+            // 
+            this.AantalPersonen.HeaderText = "Aantal Personen";
+            this.AantalPersonen.Name = "AantalPersonen";
+            // 
+            // Tijdsduur
+            // 
+            this.Tijdsduur.HeaderText = "Tijdsduur";
+            this.Tijdsduur.Name = "Tijdsduur";
+            // 
+            // Commentaar
+            // 
+            this.Commentaar.HeaderText = "Commentaar";
+            this.Commentaar.Name = "Commentaar";
+            // 
+            // GeschiedenisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Geschiedenisdata);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GeschiedenisForm";
+            this.Text = "Geschiedenis";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Geschiedenisdata)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keuzeMakenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.DataGridView Geschiedenisdata;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NaamGerecht;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Datum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AantalPersonen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tijdsduur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Commentaar;
+    }
+}
