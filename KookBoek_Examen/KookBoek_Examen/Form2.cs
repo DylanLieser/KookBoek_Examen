354ab2508d02f2a9f87c99688a4ffb966aa88bab
Joker <lieserdylan7@gmail.com>
2025-12-12 21:52:49 +0100

    Add files via upload

---
+++ KookBoek_Examen/KookBoek_Examen/Form2.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KookBoek_Examen
+{
+    public partial class KeuzeForm : Form
+    {
+        public KeuzeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void KeuzeForm_Load(object sender, EventArgs e)
+        {
+            // NumericUpDown instellen
+            Personenud.Minimum = 1;
+            Personenud.Maximum = 20;
+            Personenud.Value = 1;
+
+            // ComboBoxen alleen selecteerbaar maken (niet typen)
+            Soortdrop.DropDownStyle = ComboBoxStyle.DropDownList;
+            Vleesdrop.DropDownStyle = ComboBoxStyle.DropDownList;
+            Bijgerechtdrop.DropDownStyle = ComboBoxStyle.DropDownList;
+            Saladedrop.DropDownStyle = ComboBoxStyle.DropDownList;
+            Vermijdendrop.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Eerst alles leegmaken
+            Soortdrop.Items.Clear();
+            Vleesdrop.Items.Clear();
+            Bijgerechtdrop.Items.Clear();
+            Saladedrop.Items.Clear();
+            Vermijdendrop.Items.Clear();
+
+            // Placeholder-items (eerste keuze)
+            Soortdrop.Items.Add("Selecteer soort gerecht");
+            Vleesdrop.Items.Add("Selecteer vlees");
+            Bijgerechtdrop.Items.Add("Selecteer bijgerecht");
+            Saladedrop.Items.Add("Selecteer salade");
+            Vermijdendrop.Items.Add("Selecteer te vermijden");
+
+            // Soort gerecht
+            Soortdrop.Items.Add("Hoofdgerecht");
+            Soortdrop.Items.Add("Soep");
+            Soortdrop.Items.Add("Koud");
+            Soortdrop.Items.Add("Dessert");
+
+            // Vlees / hoofdcomponent
+            Vleesdrop.Items.Add("Biefstuk");
+            Vleesdrop.Items.Add("Kip");
+            Vleesdrop.Items.Add("Lam");
+            Vleesdrop.Items.Add("Veggie");
+
+            // Bijgerechten
+            Bijgerechtdrop.Items.Add("Puree");
+            Bijgerechtdrop.Items.Add("Rijst");
+            Bijgerechtdrop.Items.Add("Gekookte aardappelen");
+            Bijgerechtdrop.Items.Add("Pasta");
+
+            // Salades
+            Saladedrop.Items.Add("Geen");
+            Saladedrop.Items.Add("Caesar");
+            Saladedrop.Items.Add("Tomaat-komkommer");
+            Saladedrop.Items.Add("Gemengde salade");
+
+            // Vermijden
+            Vermijdendrop.Items.Add("Geen");
+            Vermijdendrop.Items.Add("Pikant");
+            Vermijdendrop.Items.Add("Vis");
+            Vermijdendrop.Items.Add("Lactose");
+
+            // Standaard selectie op placeholder
+            Soortdrop.SelectedIndex = 0;
+            Vleesdrop.SelectedIndex = 0;
+            Bijgerechtdrop.SelectedIndex = 0;
+            Saladedrop.SelectedIndex = 0;
+            Vermijdendrop.SelectedIndex = 0;
+        }
+
+
+        private void Zoekbtn_Click(object sender, EventArgs e)
+        {
+            //Waarden ophalen uit de controls
+            int aantalPersonen = (int)Personenud.Value;
+            string soortGerecht = Soortdrop.SelectedItem.ToString();
+            string vlees = Vleesdrop.SelectedItem.ToString();
+            string bijgerecht = Bijgerechtdrop.SelectedItem.ToString();
+            string salade = Saladedrop.SelectedItem.ToString();
+            string vermijden = Vermijdendrop.SelectedItem.ToString();
+
+            //Bewaar aantal personen (zodat andere forms dit kunnen gebruiken)
+            AppState.AantalPersonen = aantalPersonen;
+
+            AppState.LaatsteRecepten = RecipeGenerator.GenerateRecipes(
+                soortGerecht,
+                vlees,
+                bijgerecht,
+                salade,
+                vermijden,
+                aantalPersonen
+            );
+
+            //Ga naar recept-keuze lijst
+            ReceptKeuzeForm f = new ReceptKeuzeForm();
+            f.Show();
+            this.Hide();
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HomeForm f = new HomeForm();
+            f.Show();
+            this.Hide();
+        }
+
+        private void geschiedenisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GeschiedenisForm f = new GeschiedenisForm();
+            f.Show();
+            this.Hide();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HelpForm f = new HelpForm();
+            f.Show();
+            this.Hide();
+        }
+
+        // Zorg dat bij het sluiten van het formulier de hele app afsluit
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Als je via X sluit: app volledig afsluiten
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                Application.Exit();
+            }
+
+            base.OnFormClosing(e);
+        }
+
+        private void Vleesdrop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Soortdrop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KookBoek_Examen/KookBoek_Examen/Form4.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace KookBoek_Examen
+{
+    partial class ReceptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReceptForm));
+            this.Gerechtlbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Ingredientenbox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geschiedenisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Stappenplanbox = new System.Windows.Forms.TextBox();
+            this.Timerlbl = new System.Windows.Forms.Label();
+            this.Klaarbtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Gerechtlbl
+            // 
+            this.Gerechtlbl.AutoSize = true;
+            this.Gerechtlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gerechtlbl.Location = new System.Drawing.Point(12, 37);
+            this.Gerechtlbl.Name = "Gerechtlbl";
+            this.Gerechtlbl.Size = new System.Drawing.Size(119, 33);
+            this.Gerechtlbl.TabIndex = 0;
+            this.Gerechtlbl.Text = "Gerecht";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Ingredientenbox
+            // 
+            this.Ingredientenbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ingredientenbox.Location = new System.Drawing.Point(18, 73);
+            this.Ingredientenbox.Multiline = true;
+            this.Ingredientenbox.Name = "Ingredientenbox";
+            this.Ingredientenbox.ReadOnly = true;
+            this.Ingredientenbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Ingredientenbox.Size = new System.Drawing.Size(563, 209);
+            this.Ingredientenbox.TabIndex = 1;
+            this.Ingredientenbox.Text = "Ingredienten";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.AliceBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.geschiedenisToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(798, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.homeToolStripMenuItem.Text = "Home";
+            this.homeToolStripMenuItem.Click += new System.EventHandler(this.homeToolStripMenuItem_Click);
+            // 
+            // geschiedenisToolStripMenuItem
+            // 
+            this.geschiedenisToolStripMenuItem.Name = "geschiedenisToolStripMenuItem";
+            this.geschiedenisToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.geschiedenisToolStripMenuItem.Text = "Geschiedenis";
+            this.geschiedenisToolStripMenuItem.Click += new System.EventHandler(this.geschiedenisToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // Stappenplanbox
+            // 
+            this.Stappenplanbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stappenplanbox.Location = new System.Drawing.Point(18, 288);
+            this.Stappenplanbox.Multiline = true;
+            this.Stappenplanbox.Name = "Stappenplanbox";
+            this.Stappenplanbox.ReadOnly = true;
+            this.Stappenplanbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Stappenplanbox.Size = new System.Drawing.Size(563, 273);
+            this.Stappenplanbox.TabIndex = 3;
+            this.Stappenplanbox.Text = "Stappenplan";
+            // 
+            // Timerlbl
+            // 
+            this.Timerlbl.AutoSize = true;
+            this.Timerlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timerlbl.Location = new System.Drawing.Point(626, 463);
+            this.Timerlbl.Name = "Timerlbl";
+            this.Timerlbl.Size = new System.Drawing.Size(78, 25);
+            this.Timerlbl.TabIndex = 5;
+            this.Timerlbl.Text = "Timer: ";
+            // 
+            // Klaarbtn
+            // 
+            this.Klaarbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Klaarbtn.Location = new System.Drawing.Point(631, 491);
+            this.Klaarbtn.Name = "Klaarbtn";
+            this.Klaarbtn.Size = new System.Drawing.Size(122, 70);
+            this.Klaarbtn.TabIndex = 6;
+            this.Klaarbtn.Text = "Klaar";
+            this.Klaarbtn.UseVisualStyleBackColor = true;
+            this.Klaarbtn.Click += new System.EventHandler(this.Klaarbtn_Click);
+            // 
+            // ReceptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(798, 599);
+            this.Controls.Add(this.Klaarbtn);
+            this.Controls.Add(this.Timerlbl);
+            this.Controls.Add(this.Stappenplanbox);
+            this.Controls.Add(this.Ingredientenbox);
+            this.Controls.Add(this.Gerechtlbl);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ReceptForm";
+            this.Text = "Recept";
+            this.Load += new System.EventHandler(this.ReceptForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Gerechtlbl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox Ingredientenbox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem geschiedenisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TextBox Stappenplanbox;
+        private System.Windows.Forms.Label Timerlbl;
+        private System.Windows.Forms.Button Klaarbtn;
+    }
+}
